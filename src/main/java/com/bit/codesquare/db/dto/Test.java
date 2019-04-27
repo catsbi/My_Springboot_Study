d7e615c466a6722fcdfccce59adb2e24a4b14858
catsbi <killfine1988@gmail.com>
2019-04-27 13:47:07 +0900

    element sorting test

--- src/main/java/com/bit/codesquare/db/dto/Test.java
+++ src/main/java/com/bit/codesquare/db/dto/Test.java
@@ -1,19 +1,18 @@
 package com.bit.codesquare.db.dto;
 
 public class Test {
-	 
+
     private int id;
-    
     private String name;
- 
+
     public int getId() {
         return id;
     }
- 
+
     public void setId(int id) {
         this.id = id;
     }
- 
+
     public String getName() {
         return name;
     }
